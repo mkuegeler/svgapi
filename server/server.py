0a3de87fe7224f72264511c2d5a6b36388f8bf89
mkuegeler <mkuegeler@mac.com>
2019-12-21 23:54:15 +0100

    data.json added

--- server/server.py
+++ server/server.py
@@ -4,60 +4,36 @@ import json
 import redis
 
 application = Flask(__name__)
-# api = Api(application)
+
 api = Api(application, version='1.0', title='SVG REST API',
     description='This application provides REST access to common SVG elements and their attributes.',
 )
 
-# Sample data
-# data = {
-#     'name': 'svg',
-#     'order': 1
-# }
+# read values from json file
+def read_json_file(f):
+    # read json file
+    with open(f, "r") as read_file:
+        return json.load(read_file)
 
-el = "node"
+src = read_json_file("data/svg.json")
+svg = src["svg"]
 
 # Connect Redis database and insert sample json
-
 redis_host = 'redis'
-# db = redis.Redis(host='redis', port=6379, db=0)
+
 db = redis.StrictRedis(redis_host, port=6379, db=0)
-# db = redis.Redis(redis_host, socket_connect_timeout=1)
-# db = redis.Redis(redis_host)
-db.set('element', 'svg')
-el = str(db.get('element'))
-# el = el[2:]
-# el = str(db.keys())
-
-# for key in db.scan_iter():
-#     el = el+" - "+key
-
-try:
-    db.ping()
-    # print('Successfully connected to redis')
-    res = 'Successfully connected to redis'
-except redis.exceptions.ConnectionError as r_con_error:
-    # print('Redis connection error')
-    res = 'Redis connection error'
-    # handle exception
-
-# db.ping() 
-# print('connected to redis "{}"'.format(redis_host)) 
-
-
-# res = value+test
-# db.execute_command('JSON.SET', 'object', '.', json.dumps(data))
-# reply = json.loads(db.execute_command('JSON.GET', 'object'))
+db.set('svg', str(svg))
+el = str(db.get('svg').decode('utf-8'))
 
 @api.route('/svg')
-class HelloWorld(Resource):
+class GetSvg(Resource):
     def get(self):
-        return {'key': el}
+        return {'svg': el}
 
 @api.route('/status')
-class HelloStart(Resource):
+class GetStatus(Resource):
     def get(self):
-        return {'Status': res}
+        return {'Status': 'ok'}
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
