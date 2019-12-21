f0264db049075654fe63cb559b59c287cefff276
mkuegeler <mkuegeler@mac.com>
2019-12-21 20:49:06 +0100

    redis server added

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This application provides REST access to common SVG elements and their attributes.
 ## Installation 
 
-Clone repository with ``` git clone https://github.com/mkuegeler/svgapi.git ```, go to directory 'svgapi' ```cd svgapi``` and start it with (powerhell required):
+Clone repository with ``` git clone https://github.com/mkuegeler/svgapi.git ```, go to directory '**svgapi**' ```cd svgapi``` and start it with (Powerhell required):
 
 ```app.ps1 up ```
 
@@ -12,5 +12,13 @@ Note: If the powershell script drops a permission error, just use the plain dock
 Call the script ```app.ps1``` with sequence 'down, clean, up' to refresh changes.
 Example: ```app.ps1 down  app.ps1 clean app.ps1 up``` 
 
-## Run Applikation
+## Get updates from repository
+
+ - Go to directory '**svgapi**' ```cd svgapi```
+ - Shutdown and remove docker containers: ```app.ps1 down```
+ - Remove images: ```app.ps1 clean``` 
+ - Run: ```git push origin master```
+ - Rebuild application: ```app.ps1 up```
+
+## Run Application
 Open ```http://localhost``` in your browser.
--- server/server.py
+++ server/server.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_restplus import Resource, Api
+import json
+import redis
 
 application = Flask(__name__)
 # api = Api(application)
@@ -7,15 +9,55 @@ api = Api(application, version='1.0', title='SVG REST API',
     description='This application provides REST access to common SVG elements and their attributes.',
 )
 
+# Sample data
+# data = {
+#     'name': 'svg',
+#     'order': 1
+# }
+
+el = "node"
+
+# Connect Redis database and insert sample json
+
+redis_host = 'redis'
+# db = redis.Redis(host='redis', port=6379, db=0)
+db = redis.StrictRedis(redis_host, port=6379, db=0)
+# db = redis.Redis(redis_host, socket_connect_timeout=1)
+# db = redis.Redis(redis_host)
+db.set('element', 'svg')
+el = str(db.get('element'))
+# el = el[2:]
+# el = str(db.keys())
+
+# for key in db.scan_iter():
+#     el = el+" - "+key
+
+try:
+    db.ping()
+    # print('Successfully connected to redis')
+    res = 'Successfully connected to redis'
+except redis.exceptions.ConnectionError as r_con_error:
+    # print('Redis connection error')
+    res = 'Redis connection error'
+    # handle exception
+
+# db.ping() 
+# print('connected to redis "{}"'.format(redis_host)) 
+
+
+# res = value+test
+# db.execute_command('JSON.SET', 'object', '.', json.dumps(data))
+# reply = json.loads(db.execute_command('JSON.GET', 'object'))
+
 @api.route('/svg')
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'svg'}
+        return {'key': el}
 
-@api.route('/dev')
+@api.route('/status')
 class HelloStart(Resource):
     def get(self):
-        return {'hello': 'dev'}
+        return {'Status': res}
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
